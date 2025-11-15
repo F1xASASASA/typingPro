fd881b23f829a06c837be2ad51d2c85643c7c6f7
Ivan <132140995+PrefAir0@users.noreply.github.com>
2025-11-15 14:49:47 +0500

    Начало работ

--- src/components/TypingArea/TypingArea.tsx
+++ src/components/TypingArea/TypingArea.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import styles from './TypingArea.module.css';
 
 const TypingArea: React.FC = () => {
+  const text:string = "текст текст текст"
+    const simviols:string[] = text.split('');
+    let next_simvols:string[] = [simviols[0], simviols[1], simviols[2],simviols[3]]
+    let complete_simvols:string[] = []
+
+
+
   return (
     <div className={styles.typingArea}>
-      <input
-        type="text"
-        className={styles.typingInput}
-        placeholder=""
-        disabled
-      />
+      <div className="next_simvols">{next_simvols}</div>
+      <div className="complete_simvols">{complete_simvols}</div>
     </div>
   );
 };
