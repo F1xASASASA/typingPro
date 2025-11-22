55ef7a600034e8dd407bf290e059d57e03bd989b
Ivan <vanas0576@gmail.com>
2025-11-23 00:18:29 +0500

    pull

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,6 @@ const App: React.FC = () => {
     //       <ModeSelection />
     //       <StatsDisplay />
     //       <StartButton />
-          
     //     </main>
     //   </div>
     // </div>
