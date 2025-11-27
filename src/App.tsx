373524bc56bbd8ee1dc41434ae0470856b99eb2a
F1xASASASA <f1xasasasa@gmail.com>
2025-11-28 00:29:36 +0500

    тест1

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ const App: React.FC = () => {
     <BrowserRouter>
       <div className="vk-mini-app-main">
         <Header/>
+        
         <div className='app-wrapper'>
           <ModeSelection/>
           <main className="app-main">
