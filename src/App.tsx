b634e982ae064498a2d79bd57f24c48332956f49
Ivan <vanas0576@gmail.com>
2025-12-09 22:18:38 +0500

    Удален мусор

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import vkBridge from '@vkontakte/vk-bridge';
 import { Header, ModeSelection } from './components';
 import WelcomePage from "./components/WelcomePage/WelcomePage";
 import DailyMode from './components/modePages/DailyMode/DailyMode';
 import ClassicMode from './components/modePages/ClassicMode/ClassicMode';
-// import InfinityMode from './components/modePages/InfinityMode/InfinityMode'; // Удаляем старый
-import AIMode from './components/modePages/AIMode/AIMode'; // Добавляем новый
+import AIMode from './components/modePages/AIMode/AIMode';
 
 const App: React.FC = () => {
+
+  useEffect(() => {
+    vkBridge.send('VKWebAppInit');
+
+    vkBridge.send('VKWebAppShowBannerAd', {
+      // ИСПРАВЛЕНИЕ ЗДЕСЬ: добавляем 'as const'c
+      banner_location: 'bottom' as any
+    })
+    .then((data) => { 
+      if (data.result) {
+        console.log('Баннер успешно показан');
+      }
+    })
+    .catch((error) => {
+      console.log('Ошибка показа баннера:', error);
+    });
+  }, []);
+
+
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="vk-mini-app-main">
         <Header/>
-        
         <div className='app-wrapper'>
           <ModeSelection/>
-          <main className="app-main">
+          <main className="app-main" style={{ paddingBottom: '120px' }}>
             <Routes>
               <Route index element={<WelcomePage/>} />
               <Route path='/welcomePage' element={<WelcomePage/>}/>
               <Route path="/dailyMode" element={<DailyMode />} />
               <Route path="/classicMode" element={<ClassicMode />} />
-              {/* Меняем infinityMode на aiMode */}
               <Route path="/aiMode" element={<AIMode />} /> 
               <Route path="*" element={<WelcomePage />} />
             </Routes>
           </main>
         </div>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
